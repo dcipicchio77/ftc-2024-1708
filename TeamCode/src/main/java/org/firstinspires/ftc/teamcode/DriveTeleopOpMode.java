9ae2d88e32adbb34719df706834d4605137478e0
dcipicchio77 <daultoncipicchio77@gmail.com>
2024-11-25 20:23:31 -0500

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTeleopOpMode.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
+@TeleOp(name = "2025 drive")
+public class DriveTeleopOpMode extends LinearOpMode {
+
+    DcMotor leftElevator;
+    DcMotor rightElevator;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeft");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeft");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
+
+        leftElevator = hardwareMap.dcMotor.get("leftElevator");
+        rightElevator = hardwareMap.dcMotor.get("rightElevator");
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.left_bumper) {
+                setElevatorPower(0.6);
+            } else if (gamepad1.right_bumper) {
+                setElevatorPower(-0.6);
+            } else {
+                setElevatorPower(0);
+            }
+
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx);
+            double backLeftPower = (y - x + rx);
+            double frontRightPower = (y - x - rx);
+            double backRightPower = (y + x - rx);
+
+            frontLeftPower = Range.clip(frontLeftPower, -1, 1);
+            frontRightPower = Range.clip(frontRightPower, -1, 1);
+            backLeftPower = Range.clip(backLeftPower, -1, 1);
+            backRightPower = Range.clip(backRightPower, -1, 1);
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+        }
+    }
+    public void setElevatorPower(double power) {
+        leftElevator.setPower(power);
+        rightElevator.setPower(power);
+    }
+}
